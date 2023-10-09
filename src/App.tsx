81a62bbd8390ed0df3d7917e21c8c87f2ec29503
Taisei <taisei96.web@gmail.com>
2023-10-09 11:37:28 +0900

    ユーザーは記録を見ることができる

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Box, Button, Flex, Spacer, Text, WrapItem } from '@chakra-ui/react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Box w="600px" mx="auto" mt="100px">
+      <Text fontSize="2xl" as="b">
+        2023/10/9
+      </Text>
+      <Flex w="100%" mt={2} alignItems="center">
+        <Flex
+          w="200px"
+          h="50px"
+          mr="20px"
+          px={5}
+          alignItems="center"
+          color="#fff"
+          bg="blackAlpha.800"
+          borderRadius="5px"
+        >
+          項目名1
+        </Flex>
+        <Flex
+          w="380px"
+          h="50px"
+          mr="20px"
+          px={5}
+          alignItems="center"
+          color="#fff"
+          bg="blackAlpha.800"
+          borderRadius="5px"
+        >
+          <Spacer />
+          <Text>80kg</Text>
+          <Spacer />
+          <Text>10rep</Text>
+          <Spacer />
+        </Flex>
+
+        <Button colorScheme="red">削除</Button>
+      </Flex>
+    </Box>
+  );
 }
 
 export default App
