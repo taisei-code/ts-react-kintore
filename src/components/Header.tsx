3f408a84535009941e0e1b5871dcc3fd296fac6c
Taisei <taisei96.web@gmail.com>
2023-10-14 07:51:53 +0900

    Header実装

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,12 @@
+import { Flex, Heading} from '@chakra-ui/react'
 import React from 'react'
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
+    <Flex alignItems="center" bg="blackAlpha.800" height="80px" px={10}>
+      <Heading fontSize="3xl" color="#fff">Kintore Log</Heading>
+    </Flex>
+  );
 }
 
 export default Header
